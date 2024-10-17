91b7653fd5150ccffc7717312bad56291c8803c3
Anngelooo <AngeloColmenares00@gmail.com>
2024-10-17 17:12:05 +0200

    page loading

--- Javascript/index.js
+++ Javascript/index.js
@@ -11,10 +11,22 @@ let container = document.getElementById("container")
 getCharacters().then(characters =>{
     characters.forEach(element => {
         container.innerHTML+=`
-        <div>
-        <img src = "${element.image}">
-        <h3>${element.name}</h3>
-        </di>`
+
+        <div class="name" style="width: 18rem;">
+             <img src="${element.image}" class="image" alt="image characters">
+        <div class="name">
+            <h5 class="card-name">${element.name}</h5>
+            <h7 class="card-name">${element.race}</h7>
+        </div>
+
+      <ul class="list-group list-group-flush">
+         <li class="list-group-ki">Base Ki ${element.ki}</li>
+         <li class="list-group-maxKi">Total Ki ${element.maxKi}</li>
+         <li class="list-group-Afilliation">Afilliation ${element.affiliation}</li>
+    </ul>
+        </div>
+        
+        `
         
     });
 
